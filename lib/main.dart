ca265292aeb736e0e6771af6f97e0609f18a855e
Gutasvin <insiide123@gmail.com>
2022-03-10 14:17:47 -0300

    Alterações iniciais nas transações

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 import 'models/transaction.dart';
 
 main() => runApp(Expenses());
@@ -20,7 +20,7 @@ class MyHomePage extends StatelessWidget {
         date: DateTime.now()),
     Transaction(
         id: 't2',
-        title: 'Conta de luz de agua',
+        title: 'Conta de luz e agua',
         value: 200.00,
         date: DateTime.now()),
   ];
@@ -42,7 +42,42 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: _transactions.map((tr) {
                 return Card(
-                  child: Text(tr.title),
+                  elevation: 5,
+                  child: Row(children: [
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                          ),
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(DateFormat('d/MMM/y').format(tr.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ))
+                      ],
+                    )
+                  ]),
                 );
               }).toList(),
             )
